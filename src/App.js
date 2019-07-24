59d89cac63c861da4dddc08ae796ed215281023e
Martin Nett <martin_nett@icloud.com>
2019-07-24 09:38:24 +0800

    Split webpack config file into two env & add react support

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-family: 'Helvetica,Arial,sans-serif';
+  font-size: 24px;
+  color: palevioletred;
+`
 
 export default class App extends Component {
+  state = {
+    count: 0
+  }
+
   render() {
     return (
       <div>
-        <h1>Hello React!</h1>
+        <Title>Hello React!</Title>
+        <h2>Count: {this.state.count} </h2>
       </div>
     )
   }
