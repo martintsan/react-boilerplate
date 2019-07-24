897ca919df8965855e77157e22b603d7a3833863
Martin Nett <martin_nett@icloud.com>
2019-07-24 10:28:34 +0800

    Used CDN for react react-dom and styled-components

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,19 @@
 const merge = require('webpack-merge')
 const baseConfig = require('./webpack.config.base.js')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 module.exports = merge(baseConfig, {
-  mode: 'production'
+  mode: 'production',
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+      reportFilename: 'bundle-sizes.html'
+    })
+  ],
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+    'styled-components': 'styled'
+  }
 })
