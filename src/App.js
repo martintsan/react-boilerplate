10ffa9b60acb7bff59b0aa7d463a4c435a51c4d6
Martin Nett <martin_nett@icloud.com>
2019-07-25 14:33:51 +0800

    Finished react boilerplate

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { Component, Suspense } from 'react'
-const Warning = React.lazy(() => import('./Warning'))
-import styled, { css } from 'styled-components'
+import React, { Component } from 'react'
+import { styled } from 'styled-components'
 
 const Title = styled.h1`
   font-family: 'Helvetica,Arial,sans-serif';
@@ -8,58 +7,11 @@ const Title = styled.h1`
   color: palevioletred;
 `
 
-const Button = styled.button`
-  display: inline-block;
-  border-radius: 3px;
-  padding: 0.5rem 0;
-  margin: 0.5rem 1rem;
-  width: 11rem;
-  background: transparent;
-  color: black;
-  border: 2px solid black;
-  cursor: pointer;
-
-  ${props =>
-    props.primary &&
-    css`
-      border-color: palevioletred;
-      background: palevioletred;
-      color: white;
-    `}
-`
-
 class App extends Component {
-  state = {
-    count: 0
-  }
-
-  increment() {
-    this.setState(state => ({
-      count: state.count + 1
-    }))
-  }
-
-  decrement() {
-    this.setState(state => ({
-      count: state.count - 1
-    }))
-  }
-
   render() {
     return (
       <div>
         <Title>Hello React!</Title>
-        <h2>Count: {this.state.count} </h2>
-        {this.state.count > 10 ? (
-          <Suspense fallback={null}>
-            <Warning />
-          </Suspense>
-        ) : null}
-        <Button primary onClick={() => this.increment()}>
-          +
-        </Button>
-        <Button onClick={() => this.decrement()}>-</Button>
-        <img src="./log/png" alt="User"/>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.js'
 import '@babel/polyfill'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('app')
+)
